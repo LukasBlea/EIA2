0636558f91a8cf6c4b7f72be0b7b271370a2b103
Lukas Blea <44029295+LukasBlea@users.noreply.github.com>
2019-05-12 09:04:48 +0200

    Funktionierts jetzt?
    -

--- Aufgabe7/js/SendData.js
+++ Aufgabe7/js/SendData.js
@@ -1,7 +1,7 @@
 var L06_SendData;
 (function (L06_SendData) {
     window.addEventListener("load", init);
-    let address = "http://localhost:8100";
+    let address = "https://lukasblea-server.herokuapp.com/";
     function init(_event) {
         document.getElementById("test").addEventListener("click", buttonEvent);
     }
