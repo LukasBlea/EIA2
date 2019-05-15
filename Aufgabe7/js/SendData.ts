e8de735417de66138d4fb54e1889de437678639b
Lukas Blea <44029295+LukasBlea@users.noreply.github.com>
2019-05-16 01:41:55 +0200

    updated adress
    -

--- Aufgabe7/js/SendData.ts
+++ Aufgabe7/js/SendData.ts
@@ -2,7 +2,7 @@ namespace L06_SendData {
 
     window.addEventListener("load", init);
 
-    let address: string = "http://localhost:8100/";
+    let address: string = "https://lukasblea-server.herokuapp.com/";
 
     function init(_event: Event): void {
         document.getElementById("test").addEventListener("click", buttonEvent);
